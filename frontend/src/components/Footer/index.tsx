f0a9049ecbac36c7cb791504cacba018af2b3d6e
Filipe Souza Santos <silvertrident2@gmail.com>
2021-05-04 23:08:52 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/filipesouzasantos" target="_blank" rel="noreferrer">Filipe Souza Santos</a></p>
+          <p className="text-light"><small><strong>SILVER VENDAS</strong><br/>
+            Projeto realizado por Filipe souza Santos</small></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
