96625aee59108729d34ae6db8ec199c9efa7c5ff
Filipe Souza Santos <silvertrident2@gmail.com>
2021-05-04 22:04:13 -0300

    project created

---
+++ backend/src/main/java/com/silvertrident/silvervendas/SilvervendasApplication.java
@@ -0,0 +1,13 @@
+package com.silvertrident.silvervendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvervendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilvervendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/silvertrident/silvervendas/SilvervendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvertrident.silvervendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvervendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
