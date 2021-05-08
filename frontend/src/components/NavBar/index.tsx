ddf934078ae3bb31cbfec313c57468f20f06f461
Filipe Souza Santos <silvertrident2@gmail.com>
2021-05-07 21:30:57 -0300

    Home, Navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
     return (
         <div>
             <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
                 <div className="container">
                     <nav className="my-2 my-md-0 mr-md-3">
-                        <span >SILVER VENDAS</span>
+                        <Link to="/">
+                            <span >SILVER VENDAS</span>
+                        </Link>
                     </nav>
                 </div>
             </div>
